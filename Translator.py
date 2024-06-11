7c664c461fe9a1c1184fd7e71c3eafee0c0d7ed6
Abbas Endari <a.endari@gmail.com>
2024-06-11 22:44:39 +0330

    Translator Version 2

--- Translator.py
+++ Translator.py
@@ -1,16 +1,22 @@
 from deep_translator import GoogleTranslator
 
-with open("base.txt", "r") as basefile:
+with open("base.md", "r", encoding="utf-8") as basefile:
     words = basefile.readlines()
 for word in words:
-    word_to_translate = word.removesuffix("\n")
-    translation_en = GoogleTranslator(source="de", target="en").translate(
-        text=word_to_translate
-    )
-    translation_fa = GoogleTranslator(source="de", target="fa").translate(
-        text=word_to_translate
-    )
-    with open("text.md", "a", encoding="utf-8") as output_file:
-        output_file.write(
-            f"> [!tldr]- {word_to_translate}\n> {translation_en} \n> {translation_fa}\n\n"
+    if word.startswith("#"):
+        with open("text.md", "a", encoding="utf-8") as output_file:
+            output_file.write(f"{word}\n")
+    elif len(word)<2:
+        pass
+    else:
+        word_to_translate = word.removesuffix("\n")
+        translation_en = GoogleTranslator(source="de", target="en").translate(
+            text=word_to_translate
         )
+        translation_fa = GoogleTranslator(source="de", target="fa").translate(
+            text=word_to_translate
+        )
+        with open("text.md", "a", encoding="utf-8") as output_file:
+            output_file.write(
+                f"> [!tldr]- {word_to_translate}\n> {translation_en}\n> {translation_fa}\n\n"
+            )
